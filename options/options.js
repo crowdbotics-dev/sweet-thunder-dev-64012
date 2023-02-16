bd7f1c1fdfa85fcbd14d40f433564be69bd29a48
Crowdbotics <engineering@crowdbotics.com>
2023-02-16 04:55:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sweet-thunder-dev-64012",
+  url: "https://sweet-thunder-dev-64012.botics.co",
+  api: "https://sweet-thunder-dev-64012.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
